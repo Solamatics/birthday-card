ecf134874f925377a361fb3300dac70fdb1dc994
Olusola Afikode <afikodeolaitan@gmail.com>
2021-10-16 13:48:42 +0100

    fetch data from json and add button to clear items

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import List from './List'
+import { useState } from 'react';
+import data from './data'
 
-function App() {
+
+const App = () => {
+  const [people,setPeople] = useState(data)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <main>
+      <section className="container">
+        <List people={people}/>
+        <button onClick={() => setPeople([])}>Clear</button>
+      </section>
+    </main>
   );
 }
 
